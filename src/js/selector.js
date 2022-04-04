ea31a95429642cdb4c45888eabdcbd7b070bc996
Tojefin <tojefin@gmail.com>
2022-04-04 16:03:39 +0400

    Исправление связанного и улучшение навигации страниц

--- src/js/selector.js
+++ src/js/selector.js
@@ -1,21 +1,20 @@
-// by Tojefin
-// v1.0
-// Как использовать:
-// 1. Разметить обычный селект
-// 2. Указать id селекту
-// 3. Добавить основной класс для селекта
-// 4. В опции прописать числовой value от 0
-// 5. Классы для стилей <основной класс>__[container, list, list--open, item, item--active]
+//https://github.com/Tojefin/JS-tools
 
 //События селектов
-function selectOnChange(origin, value, oldvalue) {
-  if (value != oldvalue) {
-    if (origin.id == 'filmState') {
-      selectChangeValue(origin, 0, true)
+function selectOnChange(origin, valueId, oldvalueId) {
+  if (valueId != oldvalueId) {
+    if (origin.querySelectorAll('option')[oldvalueId]) {
+      var oldvalue = origin.querySelectorAll('option')[oldvalueId].value
+    } else {
+      var oldvalue = null
     }
-    if (origin.id == 'playerUsed') {
-      loadPlayer()
-    }
+    let value = origin.value
+    const event = new CustomEvent('selectorChange', {
+      bubbles: true,
+      detail: {value: `${value}`, oldvalue: `${oldvalue}`,
+        valueId: `${valueId}`, oldvalueId: `${oldvalueId}`}
+      });
+    origin.dispatchEvent(event);
   }
 }
 
@@ -29,13 +28,13 @@ function selectOption(option) {
 
 //Изменение значения
 function selectChangeValue(origin, value, noevent) {
-  oldvalue = origin.value
   let select = document.querySelector(`div#${origin.id}`)
   if (select) {
-    select.value = value
+    oldvalue = select.value
     select.querySelector('span').innerText = select.querySelectorAll('li')[value].innerText
+    select.value = value
   }
-  origin.value = value
+  origin.value = origin.querySelectorAll('option')[value].value
   if (!noevent) {
     selectOnChange(origin, value, oldvalue)
   }
@@ -49,39 +48,50 @@ function selectToggle(select) {
   if (list.classList.contains(`${list.classList[0]}--open`)) {
     let options = ``;
     document.querySelector(`select#${select.id}`).querySelectorAll('option').forEach((opt, i) => {
-      if (i == select.value) {
-        options = `${options}<li class="${className}__item ${className}__item--active" value="${i}" onclick="selectOption(this)">${opt.innerText}</li>`
+      if (opt.value == select.value) {
+        classList = `${className}__item ${className}__item--active`
       } else {
-        options = `${options}<li class="${className}__item" value="${i}" onclick="selectOption(this)">${opt.innerText}</li>`
+        classList = `${className}__item`
       }
+      options = `${options}<li class="${classList}" value="${i}" onclick="selectOption(this)">${opt.innerText}</li>`
     });
     list.innerHTML = `${options}`
+    select.focus();
+    select.addEventListener('focusout', () => {
+      list.classList.remove(`${list.classList[0]}--open`)
+      list.innerHTML = '';
+    }, {once: true})
+  } else {
+    list.innerHTML = '';
   }
 }
 
 // Инициализация селектов
-const selectors = document.querySelectorAll('.selector')
+const selectors = document.querySelectorAll('select')
 if (selectors) {
   if (window.innerWidth > 768) {
     selectors.forEach((item) => {
       let select = `
-          <span class="${item.classList}" onclick="selectToggle(this.parentNode)">${Array.from(item.querySelectorAll('option'))[item.value].innerText}</span>
-          <ul class="${item.classList[0]}__list">
-          </ul>
+        <span class="${item.classList}" onclick="selectToggle(this.parentNode)">${Array.from(item.querySelectorAll('option'))[0].innerText}</span>
+        <ul class="${item.classList[0]}__list"></ul>
       `
       let element = document.createElement('div');
       element.id = item.id
+      element.value = 0
+      element.tabIndex = '-1'
       element.classList = `${item.classList[0]}__container`
       element.innerHTML = select
       item.parentNode.insertBefore(element, item);
-      item.style.position = 'fixed';
-      item.style.top = '-100vh';
+      item.style.cssText = `
+        position: fixed;
+        top: -100vh;
+      `;
     });
   } else {
     selectors.forEach((item) => {
       item.addEventListener('change', (event) => {
         let origin = document.querySelector(`select#${event.target.id}`)
-        selectOnChange(origin, origin.value)
+        selectOnChange(origin, origin.value, null)
       });
     });
   }
